82c4bc7143386e10cb3c9845e45e25ce150aa2b1
Jerry Shen <jerrylshen@gmail.com>
2018-01-13 11:35:43 -0800

    89% sure

--- numpadv3.py
+++ numpadv3.py
@@ -19,13 +19,13 @@ gpio_4 = mraa.Gpio(26)
 
 # set GPIO to input
 gpio_1.dir(mraa.DIR_IN)
-#gpio_1.write(1)
+gpio_1.write(1)
 gpio_2.dir(mraa.DIR_IN)
-#gpio_2.write(1)
+gpio_2.write(1)
 gpio_3.dir(mraa.DIR_IN)
-#gpio_3.write(1)
+gpio_3.write(1)
 gpio_4.dir(mraa.DIR_IN)
-#gpio_4.write(1)
+gpio_4.write(1)
 
 i = 0
 ID = ""
@@ -33,10 +33,10 @@ ID = ""
 while True:
     i += 1
     time.sleep(1)
-    print("GPIO29 ", int(gpio_1.read()))
-    print("GPIO32 ", int(gpio_2.read()))
-    print("GPIO30 ", int(gpio_3.read()))
-    print("GPIO26 ", int(gpio_4.read()))
+    #print("GPIO29 ", int(gpio_1.read()))
+    #print("GPIO32 ", int(gpio_2.read()))
+    #print("GPIO30 ", int(gpio_3.read()))
+    #print("GPIO26 ", int(gpio_4.read()))
     if (int(gpio_1.read()) and int(gpio_2.reard())) == 1:
         ID += "1"
         print (ID)
